b1f179afc108cfbe4b93786a046090767ac0f2c6
Emely <mly.molina.minaya@gmail.com>
2018-11-06 23:36:48 -0800

    Working

--- server/index.js
+++ server/index.js
@@ -1,41 +1,43 @@
 const bodyParser = require('body-parser');
-const httpProxy = require('http-proxy');
-const apiProxy = httpProxy.createProxyServer();
-const express = require('express');
+const express    = require('express');
+const app        = express();
+// const request = require('request');
+// const http       = require('http');
+const httpProxy  = require('http-proxy');
+const proxy   = httpProxy.createProxyServer();
 const morgan = require('morgan');
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
-const app = express();
 const bannerServer = 'http://localhost:3005';
 const menuServer = 'http://localhost:3001';
-const nearbyServer = 'http://localhost:3004';
-const reviewsServer = 'http://localhost:3002';
+// const nearbyServer = 'http://localhost:3004';
+var reviewsServer = 'http://localhost:3002';
 
 app.use(morgan('dev'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/restaurants/:restaurantID/', express.static('public'));
+app.use('/restaurants/:rest_id', express.static('public'));
 
-app.get("/restaurants/:restaurantID/", function(req, res) {
-  console.log('redirecting to Server1');
-  apiProxy.web(req, res, {target: bannerServer, changeOrigin: true });
-});
+// app.all("/restaurants/:rest_id/banners/*", function(req, res) {
+//   console.log('redirecting to Server1');
+//   proxy.web(req, res, {target: bannerServer});
+// });
 
-// app.all("/app2/*", function(req, res) {
+// app.get("/grub-reactor/:id", function(req, res) {
 //   console.log('redirecting to Server2');
-//   apiProxy.web(req, res, {target: ServerTwo});
+//   apiProxy.web(req, res, {target: menuServer});
 // });
 
-// app.all("/app2/*", function(req, res) {
+// app.all("/nearby/*", function(req, res) {
 //   console.log('redirecting to Server3');
-//   apiProxy.web(req, res, {target: ServerThree});
+//   apiProxy.web(req, res, {target: nearbyServer});
 // });
 
-// app.all("/app2/*", function(req, res) {
-//   console.log('redirecting to Server3');
-//   apiProxy.web(req, res, {target: ServerThree});
-// });
+app.all("/restaurants/:rest_id/allreviews/*", function(req, res) {
+  console.log('redirecting to Server3');
+  proxy.web(req, res, {target: reviewsServer});
+});
 
 app.listen(PORT, () => {
   console.log(`node-express listening on...  ${PORT}`);
